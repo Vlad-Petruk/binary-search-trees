5b9f1ecd67524c79cd4cf8490bbdaa4fadb318e9
Vlad-Petruk <vladpetruk00@gmail.com>
2024-03-18 12:01:26 +0200

    with buildTree() and insert()

--- bst.mjs
+++ bst.mjs
@@ -1,10 +1,10 @@
-import {mergeSort, checkDuplicates} from "./sorting-array.mjs";
+import {mergeSort, checkDuplicates, prettyPrint} from "./sorting-array.mjs";
 
 class Node {
-    constructor (data, left = null, right = null) {
+    constructor (data) {
         this.data = data;
-        this.left = left;
-        this.right = right;
+        this.left = null;
+        this.right = null;
     }
 }
 
@@ -14,12 +14,38 @@ class Tree {
     }
 }
 
-function buildTree(array) {
-    let sortedArray = mergeSort(array);
-    
+
+function buildTree(array, start, end) {
+    if(start > end) {
+        return null
+    }
+
+    let mid = parseInt((start + end) / 2);
+    let node = new Node(array[mid]);
+
+    node.left = buildTree(array, start, mid - 1);
+    node.right = buildTree(array, mid + 1, end);
+    return node;
+}
+
+function insert(root, value) {
+    if (root == null) {
+        root = new Node(key);
+        return root;
+    }
+
+    if (key < root.value)
+        root.left = insert(root.left, value);
+    else if (value > root.value)
+        root.right = insert(root.right, value);
+  
+    return root;
 }
 
 let sortedArray = mergeSort([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-let noDuplicaes = checkDuplicates(sortedArray)
-console.log(sortedArray);
-console.log(noDuplicaes)
+let noDuplicates = checkDuplicates(sortedArray)
+
+let root = buildTree(noDuplicates, 0, noDuplicates.length - 1)
+// console.log(sortedArray);
+console.log(noDuplicates)
+console.log(root)
--- sorting-array.mjs
+++ sorting-array.mjs
@@ -36,4 +36,18 @@ function checkDuplicates(sortedArray) {
     return [...new Set(sortedArray)];
 }
 
-export {mergeSort, checkDuplicates}
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+ 
+
+export {mergeSort, checkDuplicates, prettyPrint}
